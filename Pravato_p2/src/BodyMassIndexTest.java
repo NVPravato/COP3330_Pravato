a17b0334302c1ab32adcc5a46db79be97fb08248
Nicholas Pravato <npravato@knights.ucf.edu>
2020-09-24 17:31:35 -0400

    Changed test class to junit 5 test class

--- Pravato_p2/src/BodyMassIndexTest.java
+++ Pravato_p2/src/BodyMassIndexTest.java
@@ -1,46 +1,52 @@
-import java.util.Scanner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-public class BodyMassIndexTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+class BodyMassIndexTest {
+    private BodyMassIndex tester;
 
 
-    public static void main(String[] args){
-        //test constructor creation
-        Scanner scan = new Scanner(System.in);
-        double height = scan.nextDouble();
-        double weight = scan.nextDouble();
-        BodyMassIndex tester = new BodyMassIndex(height, weight);
-
-        //test bmi calculation and category calculation
-        System.out.println("Height = " + height + " Weight = " + weight + " BMI: " + tester.calcBmi(height, weight) + " " + tester.calcCat(height, weight) + "\n");
-
-        //test each category
-        double heightUnder = 89;
-        double weightUnder = 200; //should print bmi of 17.8 and underweight
-
-        double heightNormal = 67;
-        double weightNormal = 152; //should print bmi of 23.8 and normal weight
-
-        double heightOver = 72;
-        double weightOver = 184; //should print bmi of 25.0 and overweight
-
-        double heightObese= 74;
-        double weightObese = 305; //should print bmi of 39.2 and obese
-
-        BodyMassIndex testerUnder = new BodyMassIndex(heightUnder, weightUnder);
-        BodyMassIndex testerNormal = new BodyMassIndex(heightNormal, weightNormal);
-        BodyMassIndex testerOver = new BodyMassIndex(heightOver, weightOver);
-        BodyMassIndex testerObese = new BodyMassIndex(heightObese, weightObese);
-
-        System.out.println("Height = " + heightUnder + " Weight = " + weightUnder + " BMI: " + tester.calcBmi(heightUnder, weightUnder) + " " + tester.calcCat(heightUnder, weightUnder));
-
-        System.out.println("Height = " + heightNormal + " Weight = " + weightNormal + " BMI: " + tester.calcBmi(heightNormal, weightNormal) + " " + tester.calcCat(heightNormal, weightNormal));
-
-        System.out.println("Height = " + heightOver + " Weight = " + weightOver + " BMI: " + tester.calcBmi(heightOver, weightOver) + " " + tester.calcCat(heightOver, weightOver));
-
-        System.out.println("Height = " + heightObese + " Weight = " + weightObese + " BMI: " + tester.calcBmi(heightObese, weightObese) + " " + tester.calcCat(heightObese, weightObese));
+    @BeforeEach
+    public void init(){
+        //BodyMassIndex tester = new BodyMassIndex(10, 20);
+    }
 
 
 
+    @Test
+    void calcBmi() {
+        tester = new BodyMassIndex(67, 152);
+        double calc = Math.round(tester.calcBmi(67, 152)*10.0)/10.0;
+        assertEquals(calc, 23.8);
+    }
+
+    @Test
+    void calcCatUnder() {
+        tester = new BodyMassIndex(89, 200);
+        String cat = tester.calcCat(89, 200);
+        assertEquals(cat, " (Underweight)");
+    }
+
+    @Test
+    void calcCatNormal() {
+        tester = new BodyMassIndex(67, 152);
+        String cat = tester.calcCat(67, 152);
+        assertEquals(cat, " (Normal Weight)");
+    }
+
+    @Test
+    void calcCatOver() {
+        tester = new BodyMassIndex(72, 184);
+        String cat = tester.calcCat(72, 184);
+        assertEquals(cat, " (Overweight)");
+    }
+
+    @Test
+    void calcCatObese() {
+        tester = new BodyMassIndex(74, 305);
+        String cat = tester.calcCat(74, 305);
+        assertEquals(cat, " (Obese)");
     }
 
 }
